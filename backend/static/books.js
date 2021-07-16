4a059d782700ff602219795475caf41008dfe256
sunnymoon <drornob@gmail.com>
2021-07-16 11:41:31 +0600

    initial commit

---
+++ backend/static/books.js
@@ -0,0 +1,95 @@
+(document.getElementById("download_image_1").onclick = function () {
+    location.href = "/an_introduction_to_programming_with_bash";
+}),
+    (document.getElementById("download_image_2").onclick = function () {
+        location.href = "/answering_those_who_altered_the_religion_of_jesus_christ";
+    }),
+    (document.getElementById("download_image_3").onclick = function () {
+        location.href = "/apache_sqoop_cookbook";
+    }),
+    (document.getElementById("download_image_4").onclick = function () {
+        location.href = "/basic_english_grammar";
+    }),
+    (document.getElementById("download_image_5").onclick = function () {
+        location.href = "/begining_with_node.js";
+    }),
+    (document.getElementById("download_image_6").onclick = function () {
+        location.href = "/c_programming_hsc_cracker";
+    }),
+    (document.getElementById("download_image_7").onclick = function () {
+        location.href = "/c_programming_hsc_cracker_2";
+    }),
+    (document.getElementById("download_image_8").onclick = function () {
+        location.href = "/c_programming_loops";
+    }),
+    (document.getElementById("download_image_9").onclick = function () {
+        location.href = "/physics_chapter_4";
+    }),
+    (document.getElementById("download_image_10").onclick = function () {
+        location.href = "/physics_chapter_5";
+    }),
+    (document.getElementById("download_image_11").onclick = function () {
+        location.href = "/computer_programming_subin";
+    }),
+    (document.getElementById("download_image_12").onclick = function () {
+        location.href = "/css_beginner_guide";
+    }),
+    (document.getElementById("download_image_13").onclick = function () {
+        location.href = "/rest_api_nodejs";
+    }),
+    (document.getElementById("download_image_14").onclick = function () {
+        location.href = "/dynamics";
+    }),
+    (document.getElementById("download_image_15").onclick = function () {
+        location.href = "/earn_money_10min";
+    }),
+    (document.getElementById("download_image_16").onclick = function () {
+        location.href = "/hacking_security";
+    }),
+    (document.getElementById("download_image_17").onclick = function () {
+        location.href = "/html_beginner";
+    }),
+    (document.getElementById("download_image_18").onclick = function () {
+        location.href = "/hunt_python";
+    }),
+    (document.getElementById("download_image_19").onclick = function () {
+        location.href = "/javascript_beginner_guide";
+    }),
+    (document.getElementById("download_image_20").onclick = function () {
+        location.href = "/ml_cheatsheet";
+    }),
+    (document.getElementById("download_image_21").onclick = function () {
+        location.href = "/mastering_python";
+    }),
+    (document.getElementById("download_image_22").onclick = function () {
+        location.href = "/ml_cheatsheet_two";
+    }),
+    (document.getElementById("download_image_23").onclick = function () {
+        location.href = "/mysql_beginner_guide";
+    }),
+    (document.getElementById("download_image_24").onclick = function () {
+        location.href = "/linear_algebra_notes";
+    }),
+    (document.getElementById("download_image_25").onclick = function () {
+        location.href = "/organic_chemistry_notes";
+    }),
+    (document.getElementById("download_image_26").onclick = function () {
+        location.href = "/php_cookbook";
+    }),
+    (document.getElementById("download_image_27").onclick = function () {
+        location.href = "/physics_beat_intensity";
+    }),
+    (document.getElementById("download_image_28").onclick = function () {
+        location.href = "/statistics_note";
+    }),
+    (document.getElementById("download_image_29").onclick = function () {
+        location.href = "/think_like_a_monk";
+    }),
+    (document.getElementById("download_image_30").onclick = function () {
+        location.href = "/web_roadmap";
+    }),
+    $(function () {
+        var n = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+            o = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        $("html, body").css({ width: n, height: o });
+    });
